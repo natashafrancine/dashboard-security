8eb6760e99176c2d356636faa17eb40614b4968f
natashafrancine <natashafrancinenazarino@gmail.com>
2025-11-10 22:52:50 +0800

    done

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -1,25 +1,16 @@
 <?php
-// src/Controller/DashboardController.php
+
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController
+class DashboardController extends AbstractController
 {
   #[Route('/dashboard', name: 'dashboard')]
   public function index(): Response
   {
-    $html = <<<HTML
-<!doctype html>
-<html>
-  <head><meta charset="utf-8"><title>Dashboard</title></head>
-  <body>
-    <p>Welcome to the dashboard! (Protected Area)</p>
-  </body>
-</html>
-HTML;
-
-    return new Response($html);
+    return $this->render('dashboard.html.twig');
   }
 }
